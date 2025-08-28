aa3ca77f9dc299f102f85aec761de3c7ef0fd0fa
Samuel Colvin <s@muelcolvin.com>
2025-08-28 11:38:54 -0400

    look at LambdaRuntimeClient

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
-from pprint import pprint
+import sys
 
 import logfire
 from fastmcp import FastMCP
 
-# env = 'local'
-env = 'prod'
+env = 'prod' if 'AWS_LAMBDA_FUNCTION_NAME' in os.environ else 'local'
 logfire.configure(service_name=env, environment=env)
 
 logfire.instrument_mcp()
@@ -16,7 +15,17 @@ mcp = FastMCP('Hello World')
 @mcp.tool
 def hello(name: str) -> str:
     logfire.info('env', environ=dict(os.environ))
-    pprint(dict(os.environ))
+
+    for mod_name, mod in list(sys.modules.items()):
+        try:
+            client = getattr(mod, 'LambdaRuntimeClient', None)
+        except Exception:  # pragma: no cover
+            continue
+        if not client:
+            continue
+        logfire.info('LambdaRuntimeClient:', mod_name=mod_name, client=client)
+
+    logfire.force_flush()
     return f'Hello, {name}!'
 
 
